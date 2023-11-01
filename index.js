7ee17fab395b1ab1a7409be20ed6a4f00224446d
Martin Georgiev <geor0020@hz.nl>
2023-11-01 12:39:28 +0100

    testing

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const path = require('path'); // Import the path module
+const app = express();
+const port = 3000;
+
+// Serve static files from the current directory
+app.use(express.static(__dirname)); // Use __dirname to refer to the current directory
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+  res.sendFile(path.join(__dirname, 'index.html')); // Serve the index.html file
+});
 
 app.listen(port, () => {
-    console.long(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
