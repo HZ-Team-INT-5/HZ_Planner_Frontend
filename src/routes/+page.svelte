44d5e0dada7ddd17482cda1bcfbb9fe622e62d83
Martin Georgiev <geor0020@hz.nl>
2023-11-27 10:49:18 +0100

    Modified main routes directory page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<a class="courses" href="/courses">Course Overview</a>
+<a class="notes" href="/notes">Notes</a>
+<a class="calendar" href="/calendar">Calendar</a>
+
+<style>
+    .courses {
+        border: 2px solid black;
+        color: black;
+	}
+    .notes {
+        border: 2px solid black;
+        color: black;
+	}
+    .calendar {
+        border: 2px solid black;
+        color: black;
+	}
+</style>
