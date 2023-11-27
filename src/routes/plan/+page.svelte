12af7eb717778941c279ad001f7cebfffb84a4b1
Milton Nji Fon <fon0001@hz.nl>
2023-11-27 14:40:50 +0100

    update

---
+++ src/routes/plan/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+    import FloorPlan from '../components/FloorPlan.svelte';
+  </script>
+  
+  <style>
+    /* Business-level styling with blue theme */
+    h1 {
+      text-align: center;
+      color: #0073e6;
+      margin-bottom: 20px;
+    }
+  
+    .container {
+      max-width: 1200px;
+      margin: 0 auto;
+      padding: 20px;
+      background-color: #f0f5ff;
+      border: 1px solid #b3d9ff;
+      border-radius: 8px;
+    }
+  </style>
+  
+  <div class="container">
+    <h1>Building Floor Plans</h1>
+    <FloorPlan />
+  </div>
