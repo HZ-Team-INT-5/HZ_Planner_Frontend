5e2fd556e66a3a6c702f29c5e61fd97c59955ef5
James Nzungize <60880312+MajesNth@users.noreply.github.com>
2023-11-28 08:26:17 -0500

    changed css for read notifications,  unread notifications, and css table
    read notifications before: dark green background, white text
    unread notifications before: dark red background, white text
    
    read notifications after: transparent background, black text
    unread notifications after: circle on the right, blue background, black bold text
    legend table after: added black borders

--- src/routes/notifications/+page.svelte
+++ src/routes/notifications/+page.svelte
@@ -47,31 +47,48 @@
 	<br />
 	<table class="legend">
 		<th>Legend</th>
+
 		<tr>
-			<p class="notification read">Read notifications</p>
+			<td class="read-td">
+				<ul>
+					<li class="notification read">Read notifications</li>
+				</ul>
+			</td>
 		</tr>
 		<tr>
-			<p class="notification unread">Unread notifications</p>
+			<td>
+				<ul>
+					<li class="notification unread">Unread notifications</li>
+				</ul>
+			</td>
 		</tr>
 	</table>
 </body>
 
 <style>
 	:root {
-    --unread-background-color:rgba(165, 42, 42, 0.85);
-    --read-background-color: rgba(0, 100, 0, 0.8);
-	--notification-font-color:white;
-	--notification-li-border-radius:5px;
-	--notification-li-border-padding:5px;
-	--notification-li-border-margin:2px;
-	--font-color:#3798db;
-  }
+		/* Page in general */
+		--page-background-color: #f0f5f9;
+		--font-color: rgb(55, 152, 219);
+		/* Notification li variables*/
+		--notification-li-border-radius: 5px;
+		--notification-li-border-padding: 5px;
+		--notification-li-border-margin: 2px;
+		--unread-list-style-type: circle;
+		/* Notification colors */
+		--unread-background-color: rgba(108, 194, 255, 0.85);
+		--read-background-color: rgba(0, 0, 0, 0);
+		--unread-notification-font-color: black;
+		--read-notification-font-color: black;
+		/* Legend Table */
+		--legend-table-border:1px solid black;
+	}
 	body {
 		margin: 0px;
-		background-color: #f0f5f9;
+		background-color: var(--page-background-color);
 	}
 	h2 {
-		color:  var(--font-color);
+		color: var(--font-color);
 		padding: 10px;
 		margin-top: 0px;
 		margin-left: 0px;
@@ -81,6 +98,7 @@
 	}
 	.notification_li_div {
 		width: 80%;
+		max-width: 800px;
 		/* backgrond color used for debugging purposes */
 		/* background-color:red; */
 	}
@@ -90,30 +108,43 @@
 	}
 	.notification_li {
 		max-width: 100%;
-		list-style-type: none;
 		/* border: solid 1px black; */
 		border-radius: var(--notification-li-border-radius);
 		padding: var(--notification-li-border-padding);
 		margin: var(--notification-li-border-margin);
+		list-style-position: outside;
 	}
 	.unread {
-		color: var(--notification-font-color);
+		list-style-type: var(--unread-list-style-type);
+		font-weight: bold;
+		color: var(--unread-notification-font-color);
 		background-color: var(--unread-background-color);
 	}
 	.read {
-		color:var(--notification-font-color);
+		list-style-type: none;
+		color: var(--read-notification-font-color);
 		background-color: var(--read-background-color);
 	}
 	.legend {
 		margin-left: 20px;
-		width: min-content;
+		width: max-content;
+		border: var(--legend-table-border);
 	}
 	.legend th {
 		color: var(--font-color);
+		border-bottom: var(--legend-table-border);
+	}
+	.legend .read-td{
+		border-bottom: var(--legend-table-border);
 	}
 	.legend .notification {
 		border-radius: var(--notification-li-border-radius);
 		padding: var(--notification-li-border-padding);
 		margin: var(--notification-li-border-margin);
+	}
+	.ul .legend {
+		border: 0px;
+		margin: 0px;
+		padding: 0px;
 	}
 </style>
