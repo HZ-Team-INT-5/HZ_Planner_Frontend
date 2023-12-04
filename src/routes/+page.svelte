4bbd793b7b66ab7eea39a8cbd88f8941198c1492
michalpij <jaku0001@hz.nl>
2023-12-04 11:36:52 +0100

    Remade the homescreen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,11 +19,9 @@
 
 		main {
 			flex: 1;
-			padding: 20px;
 			display: flex;
 			flex-wrap: wrap;
 			justify-content: center;
-			gap: 20px;
 		}
 
 		.welcome-message {
@@ -61,78 +59,52 @@
 			margin-bottom: 20px;
 		}
 
-		footer {
-			background-color: #3498db;
-			color: #ffffff;
-			text-align: center;
-			padding: 15px;
+		.wrap {
 			width: 100%;
-			height: 50px;
+		}
+
+		.calendar {
+		    background-color: #3498db;
+			float: left;
+			width: 65%;
+			height: 1000px;
+		}
+
+		.courses {
+			background-color: white;
+			float: right;
+			width: 35%;
+			height: 1000px;
 		}
 
 		.hidden {
 			display: none;
+		}
+
+		footer {
+			background-color: #d1e2ee;
+			color: #3498db;
+			text-align: center;
+			padding-top: 10px;
+			padding-bottom: 5px;
+			width: 100%;
+			height: 60px;
 		}
 	</style>
 </head>
 
 <body>
 	<main>
-		<div class="welcome-message">
-			<p>Welcome to the HZ Planner for first year ICT students at the HZ!</p>
-			<p>Explore the pages of our book below:</p>
-		</div>
-		<div class="logo-container">
-			<img src="logo.jpg" alt="HZ Planner Logo" />
-		</div>
-		<div class="cards-section">
-			<section class="card">
-				<h2>Calendar</h2>
-				<a class="calendar" href="/calendar">Calendar</a>
-			</section>
-
-			<section class="card">
-				<h2>Course Overview</h2>
-				<a class="courses" href="/courses">Course Overview</a>
-			</section>
-
-			<section class="card">
-				<h2>Notes</h2>
-				<a class="notes" href="/notes">Notes</a>
-			</section>
-
-			<section class="card">
-				<h2>Floor Plan</h2>
-				<a class="floorplan" href="/floorplan">Floor Plan</a>
-			</section>
-
-			<section class="card">
-				<h2>Notifications</h2>
-				<a class="notifications" href="/notifications">Notifications</a>
-			</section>
-
-			<section class="card, hidden">
-				<h2>Card 6</h2>
-				<p>Content for Card 6...</p>
-			</section>
-
-			<section class="card, hidden">
-				<h2>Card 7</h2>
-				<p>Content for Card 7...</p>
-			</section>
-
-			<section class="card, hidden">
-				<h2>Card 8</h2>
-				<p>Content for Card 8...</p>
-			</section>
-
-			<section class="card, hidden">
-				<h2>Card 9</h2>
-				<p>Content for Card 9...</p>
-			</section>
-		</div>
+		<div class="wrap">
+		    <div class="calendar">
+			    <a href="/calendar">Calendar</a>
+		    </div>
+		    <div class="courses">
+			    <a href="/courses">Course Overview</a>
+		    </div>
+	    </div>
 		<footer>
 			<p>&copy; 2023 HZ Planner. All rights reserved.</p>
 		</footer>
-	</main></body
->
+	</main>
+</body>
