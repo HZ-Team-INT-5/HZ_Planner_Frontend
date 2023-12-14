51892befd39008701caf2dc91cd4b1eda03e0cba
James <60880312+MajesNth@users.noreply.github.com>
2023-12-14 12:23:29 +0100

    Create Popup.svelte

---
+++ src/routes/courses/Popup.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+	export let show: boolean = false;
+    export let popupData: string; 
+
+	function closePopup() {
+		show = false;
+	}
+</script>
+
+<div class="popup" style={show ? 'display: block;' : 'display: none;'}>
+	<div class="popup-content">
+		<span class="close" on:click={closePopup}>&times;</span>
+        <p>{@html popupData}</p>
+	</div>
+</div>
+
+<style>
+	/* Add your styling for the popup here */
+	.popup {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background: rgba(0, 0, 0, 0.5);
+		display: none;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.popup-content {
+        top: 10%;
+		left: auto;
+        width: 70%;
+        margin:auto auto;
+        justify-content: center;
+		align-items: center;
+		background: #fff;
+		padding: 20px;
+		border-radius: 5px;
+		position: relative;
+	}
+
+	.close {
+		position: absolute;
+		top: 10px;
+		right: 10px;
+		cursor: pointer;
+	}
+</style>
