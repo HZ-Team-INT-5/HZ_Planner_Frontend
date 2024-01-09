99ff6a8d308d4ec33014e9165b51cbbead89329d
Tangobyte <112774024+Tangobyte@users.noreply.github.com>
2024-01-10 00:42:29 +0100

    updated login

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,10 +1,35 @@
 <script>
-	import { enhance } from '$app/forms'
-	export let form
+  import { onMount } from 'svelte';
+  import { navigate } from 'svelte-routing';
+  import axios from 'axios';
+
+  let formData = {
+    username: '',
+    password: '',
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const response = await axios.post('http://localhost:5173/api/login', formData);
+      console.log('Login successful!', response.data);
+
+      // Redirect to a protected page after successful login
+      navigate('/'); // Replace '/dashboard' with your desired route
+    } catch (error) {
+      console.error('Login failed', error);
+    }
+  };
 </script>
 
-<form method="post" use:enhance>
-  <input name="email" value={form?.email ?? ''}/>
-  <input type="password" name="password"/>
-  <button type="submit">Sign up</button>
-</form>
+<main>
+  <h1>Login Page</h1>
+  <form on:submit|preventDefault={handleSubmit}>
+    <label for="username">Username:</label>
+    <input type="text" id="username" bind:value={formData.username} />
+
+    <label for="password">Password:</label>
+    <input type="password" id="password" bind:value={formData.password} />
+
+    <button type="submit">Login</button>
+  </form>
+</main>
