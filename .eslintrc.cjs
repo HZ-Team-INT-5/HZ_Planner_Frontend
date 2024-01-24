d3912933e0374dc0e61e8849393aa082e4fdfb75
Milton Nji Fon <fon0001@hz.nl>
2024-01-24 10:15:35 +0100

    added linter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,30 +1,44 @@
 module.exports = {
 	root: true,
 	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:svelte/recommended',
-		'prettier'
+	  'eslint:recommended',
+	  'plugin:@typescript-eslint/recommended',
+	  'plugin:svelte/recommended',
+	  'prettier',
 	],
 	parser: '@typescript-eslint/parser',
 	plugins: ['@typescript-eslint'],
 	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2020,
-		extraFileExtensions: ['.svelte']
+	  sourceType: 'module',
+	  ecmaVersion: 2020,
+	  extraFileExtensions: ['.svelte'],
 	},
 	env: {
-		browser: true,
-		es2017: true,
-		node: true
+	  browser: true,
+	  es2017: true,
+	  node: true,
 	},
 	overrides: [
-		{
-			files: ['*.svelte'],
-			parser: 'svelte-eslint-parser',
-			parserOptions: {
-				parser: '@typescript-eslint/parser'
-			}
-		}
-	]
-};
+	  {
+		files: ['*.svelte'],
+		parser: 'svelte-eslint-parser',
+		parserOptions: {
+		  parser: '@typescript-eslint/parser',
+		},
+		rules: {
+		  'indent': ['error', 2],
+		  'space-infix-ops': ['error', { 'int32Hint': false }],
+		  'quotes': ['error', 'single'],
+		  'semi': ['error', 'always'],
+		  'prefer-const': ['error', { 'destructuring': 'all' }],
+		  'func-style': ['error', 'expression', { 'allowArrowFunctions': true }],
+		  'no-unused-vars': ['error', { 'vars': 'all', 'args': 'after-used', 'ignoreRestSiblings': false }],
+		  'object-curly-spacing': ['error', 'always'],
+		  'svelte/onclick-style': ['error', 'arrow'],
+		  'svelte/component-name': ['error', { 'componentName': '^[A-Z][a-zA-Z]*$' }],
+		  'svelte/no-else-return': ['error'],
+		},
+	  },
+	],
+  };
+  
